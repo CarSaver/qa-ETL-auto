e400696cdf3ca1e0da0a1cca98af1bf1faacd4df
jtucker <j.e.tucker.valdes@gmail.com>
2022-10-28 07:52:29 -0400

    Initial

--- qa-ETL-auto/src/main/java/com/carsaver/example/Main.java
+++ qa-ETL-auto/src/main/java/com/carsaver/example/Main.java
@@ -1,20 +1,35 @@
 package com.carsaver.example;
 
+import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.model.Bucket;
 import software.amazon.awssdk.services.s3.model.ListBucketsResponse;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+import software.amazon.awssdk.services.s3.model.PutObjectResponse;
+
+import java.io.File;
 
 public class Main {
     public static void main(String[] args) {
         fileUploadToAWSS3();
     }
 
+    private static final String bucket = "upgradeprospectetl-bucket-ingestion-staging";
+    private static final String runId = "0bb5c200-6a4e-4f94-b8ad-1df4524eada1";
+    private static final String campaignId = "17260442-9dd8-4a40-a6ab-6f40cfc0cb20";
+    private static final String tableName = "tableName";
+
+    private static final String path = runId + "/" + campaignId + "/" + tableName;
+
     public static void fileUploadToAWSS3() {
         S3Client s3 = S3Client.create();
-        ListBucketsResponse response = s3.listBuckets();
-        for (Bucket bucket : response.buckets()) {
-            System.out.println(bucket.name());
-        }
+
+        PutObjectResponse putObjectResponse = s3.putObject(PutObjectRequest.builder()
+                .bucket(bucket).key(path + "/prospectData.csv")
+                .build(), RequestBody.fromFile(new File("src/main/resources/prospectData.csv"))
+        );
+
+        System.out.println(putObjectResponse);
     }
 }
 
